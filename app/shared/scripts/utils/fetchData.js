d134730b91c2a114c05ac6df6a7cc66fb6431756
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-03-18 20:30:59 +0000

    melhora logica interna permitindo passar options e tornando erro em casos onde o response.ok nao seja de sucesso

--- app/shared/scripts/utils/fetchData.js
+++ app/shared/scripts/utils/fetchData.js
@@ -1,10 +1,16 @@
 import { API_BASE_URL } from "../api/api_url.js";
 
-export async function fetchData(endpoint) {
+export async function fetchData(endpoint, opcoes = {}) {
   try {
-    const response = await fetch(`${API_BASE_URL}/${endpoint}`);
+    const response = await fetch(`${API_BASE_URL}/${endpoint}`, opcoes);
 
-    return response.json();
+    if (!response.ok) {
+      throw new Error(`Erro ${response.status}: ${response.statusText}`);
+    }
+
+    // No caso de DELETE ou outros casos que a gente nao retorna body
+    const contentType = response.headers.get("content-type");
+    return contentType?.includes("application/json") ? response.json() : null;
   } catch (err) {
     console.log(`Falha ao tentar carregar dados de ${endpoint}: ${err}`);
   }
