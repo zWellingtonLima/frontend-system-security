ffee1b0861dcbe2b6b10a1c1fa0f62b94d8b67a3
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-03-18 08:12:18 +0000

    melhora logica de lists criando funcao unica para reaproveitar a logica do fetch

--- app/shared/scripts/api/lists.js
+++ app/shared/scripts/api/lists.js
@@ -1,55 +1,38 @@
 const API_BASE_URL = "http://localhost:8080/api";
 
 // Listagem de FUNCIONARIOS
-fetch(`${API_BASE_URL}/funcionarios`)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+fetchData("funcionarios");
 
 // Listagem de VISITANTES
-fetch(`${API_BASE_URL}/visitantes`)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+fetchData("visitantes");
 
-// Listagem de TIPOS DE CONSUMO
-fetch(`${API_BASE_URL}/lists/tipos-consumo`)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+// Listagem de TIPOS DE CONSUMO;
+fetchData("lists/tipos-consumo");
 
-// Listagem de TIPOS DE CONSUMO
-fetch(`${API_BASE_URL}/lists/tipos-chave`)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+// Listagem de TIPOS DE CONSUMO;
+fetchData("lists/tipos-chave");
 
 // Listagem de TIPOS OCORRENCIA
-fetch(`${API_BASE_URL}/lists/tipos-ocorrencia`)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+fetchData("lists/tipos-ocorrencia");
 
 // Listagem de MOVIMENTACOES ATIVAS
-fetch(`${API_BASE_URL}/lists/movimentacoes/ativas`)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+fetchData("lists/movimentacoes/ativas");
 
 // Listagens todas as MOVIMENTACOES
-fetch(`${API_BASE_URL}/lists/movimentacoes/ativas`)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+fetchData("lists/movimentacoes");
 
 // Listagens todas as MOVIMENTACOES VISITANTES ID
-fetch(`${API_BASE_URL}/lists/movimentacoes/visitante/1`)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+fetchData("lists/movimentacoes/visistante/1");
 
 // Listagens todas as MOVIMENTACOES FUNCIONARIOS ID
-fetch(`${API_BASE_URL}/lists/movimentacoes/funcionario/1`)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+fetchData("lists/movimentacoes/funcionario/1");
+
+async function fetchData(endpoint) {
+  try {
+    const response = await fetch(`${API_BASE_URL}/${endpoint}`);
+
+    return response.json();
+  } catch (err) {
+    console.log(`Falha ao tentar carregar dados de ${endpoint}: ${err}`);
+  }
+}
