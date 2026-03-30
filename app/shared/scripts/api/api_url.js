6b4e53b019a620cf02d653d80bd3f773aa5be193
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-03-30 12:51:09 +0100

    adiciona url teste para producao

--- app/shared/scripts/api/api_url.js
+++ app/shared/scripts/api/api_url.js
@@ -1,1 +1,5 @@
 export const API_BASE_URL = "http://localhost:8080/api";
+
+// API URL Produção
+// export const API_BASE_URL =
+//   "https://backend-system-security-production.up.railway.app/api";
