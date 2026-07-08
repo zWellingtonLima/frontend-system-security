fec011ce1885ab4a94a3aeebb99a569ba15dfb0a
zWellingtonLima <0wellingtonlima@gmail.com>
2026-07-08 21:03:38 +0100

    feat: cria módulo de portaria já com roteamento base

--- src/app/dashboard/portaria/portaria-routing.module.ts
+++ src/app/dashboard/portaria/portaria-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ConsumosComponent } from "./components/consumos/consumos.component";
+import { OcorrenciasComponent } from "./components/ocorrencias/ocorrencias.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "consumos",
+    component: ConsumosComponent,
+  },
+  {
+    path: "ocorrencias",
+    component: OcorrenciasComponent,
+  },
+  {
+    path: "",
+    redirectTo: "/ocorrencias",
+    pathMatch: "full",
+  },
+  {
+    path: "**",
+    redirectTo: "ocorrencias",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PortariaRoutingModule { }
+export class PortariaRoutingModule {}
--- src/app/dashboard/portaria/portaria.module.ts
+++ src/app/dashboard/portaria/portaria.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from "@angular/common";
 import { PortariaRoutingModule } from "./portaria-routing.module";
 import { PortariaComponent } from "./portaria.component";
 import { SharedModule } from "src/app/shared/shared.module";
+import { ConsumosComponent } from './components/consumos/consumos.component';
+import { OcorrenciasComponent } from './components/ocorrencias/ocorrencias.component';
 
 @NgModule({
   imports: [CommonModule, PortariaRoutingModule, SharedModule],
-  declarations: [PortariaComponent],
+  declarations: [PortariaComponent, ConsumosComponent, OcorrenciasComponent],
   exports: [PortariaComponent],
 })
 export class PortariaModule {}
