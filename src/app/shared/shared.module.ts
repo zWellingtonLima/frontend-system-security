662dffe6127b289003b4674e956ccb94ccb28675
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-07-14 12:05:08 +0100

    feat: adiciona componente compartilhado de modal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { TopbarComponent } from "./components/topbar/topbar.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule } from "@angular/router";
 import { FormsModule } from "@angular/forms";
-import { PortariaShellComponent } from './components/portaria-shell/portaria-shell.component';
+import { PortariaShellComponent } from "./components/portaria-shell/portaria-shell.component";
+import { BaseModalComponent } from "./components/base-modal/base-modal.component";
 
 @NgModule({
-  imports: [CommonModule, RouterModule, FormsModule],
-  declarations: [NavbarComponent, TopbarComponent, PortariaShellComponent],
-  exports: [NavbarComponent, TopbarComponent, PortariaShellComponent],
+  imports: [CommonModule, RouterModule, FormsModule, BrowserAnimationsModule],
+  declarations: [
+    NavbarComponent,
+    TopbarComponent,
+    PortariaShellComponent,
+    BaseModalComponent,
+  ],
+  exports: [
+    NavbarComponent,
+    TopbarComponent,
+    PortariaShellComponent,
+    BaseModalComponent,
+  ],
 })
 export class SharedModule {}
